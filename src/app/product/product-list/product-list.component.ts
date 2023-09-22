92bce37a6b64df6cdf17b421e4687f538484b548
Gerardo Villafana <lg06luisgerardo@gmail.com>
2023-09-21 18:48:42 -0600

    21/09/2023-LGVC

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  imagenWidth:number=50;
+  imagenMargin:number=2;
+  muestraImg:boolean=true;
+  listFiltrer:string='';
+
+
+  productos:any[]=[
+  {
+    "productoId":1,
+    "Modelo":"Sentra",
+    "Descripcion":"4 puertas familiar",
+    "year":"febrero 3 2020",
+    "precio":12000,
+    "Marca":"NISSAN",
+    "Color":"Verde",
+    "imagenUrl":"https://st1.uvnimg.com/d4/4a/006304a74db4902c0b4d8d8026c8/chevrolet-corvette-c8-stingray-2020-1280-08.jpg"
+
+  },
+  {
+    "productoId":2,
+    "Modelo":"Tsuru",
+    "Descripcion":"4 puertas chicas ",
+    "year":"Enero 2 2020",
+    "precio":15000,
+    "Marca":"TOYOTA",
+    "Color":"AZUL",
+    "imagenUrl":"https://loscoches.com/wp-content/uploads/2021/04/carros-deportivos-potencia.jpg"
+
+  },
+  {
+    "productoId":3,
+    "Modelo":"Chevy",
+    "Descripcion":"2 puertas",
+    "year":"Diciembre 2015",
+    "precio":50000,
+    "Marca":"NISSAN",
+    "Color":"Rojo",
+    "imagenUrl":"https://static.retail.autofact.cl/blog/c_img_740x370.9j7kj78laudtwc5.jpg"
+  },
+  ]
+}
